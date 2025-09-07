5fe9a6fc5cfb1d79fdab67302c33b0082e1d0d40
Ryota <lacoru43oo@gmail.com>
2025-09-07 21:07:30 +0900

    fix: Add middleware to prevent redirect loop

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,22 @@
-export { default } from 'next-auth/middleware';
+import { withAuth } from "next-auth/middleware";
 
-// この設定により、指定したパスが保護対象となります
+export default withAuth({
+  pages: {
+    signIn: '/login',
+  },
+});
+
+// この設定で、どのページを認証保護の対象にするかを定義します
 export const config = {
-  matcher: ['/nfc/:path*'],
+  matcher: [
+    /*
+     * 以下のパスを除く、すべてのリクエストパスを認証の対象とする
+     * - /api/ (APIルート)
+     * - /_next/static (静的ファイル)
+     * - /_next/image (画像最適化ファイル)
+     * - /favicon.ico (ファビコンファイル)
+     * - /login (ログインページ)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|login).*)",
+  ],
 };
